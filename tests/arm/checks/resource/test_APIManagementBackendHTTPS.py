080f116589d677a7be7e0f127968aa5709511760
rutiNalenger <frutiz2641@gmail.com>
2024-06-18 15:07:25 +0300

    APIManagementBackendHTTPS

---
+++ tests/arm/checks/resource/test_APIManagementBackendHTTPS.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+
+from checkov.arm.checks.resource.APIManagementBackendHTTPS import check
+from checkov.arm.runner import Runner
+from checkov.runner_filter import RunnerFilter
+
+
+class TestAKSRbacEnabled(unittest.TestCase):
+
+    def test_summary(self):
+        runner = Runner()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        test_files_dir = current_dir + "/example_APIManagementBackendHTTPS"
+        report = runner.run(root_folder=test_files_dir,runner_filter=RunnerFilter(checks=[check.id]))
+        summary = report.get_summary()
+
+        self.assertEqual(summary['passed'], 1)
+        self.assertEqual(summary['failed'], 1)
+        self.assertEqual(summary['skipped'], 0)
+        self.assertEqual(summary['parsing_errors'], 0)
+        self.assertEqual(summary.get('unknown', 0), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
